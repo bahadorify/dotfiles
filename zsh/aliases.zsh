c31534a8b63b96dc3a47d3af6cd2bf6c5f69d9c1
Bahador Najafiazar <69297482+bahadorify@users.noreply.github.com>
2023-03-12 14:28:53 +0100

    Add Git branch remote unused delete

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,6 +22,8 @@ alias dcd='docker-compose down'
 
 # Git
 alias gsm='git-selective-merge'
+alias gbrud='git fetch -p ; git branch -r | awk '"'"'{print $1}'"'"' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '"'"'{print $1}'"'"' | xargs git branch -d'
+
 
 # Redis
 alias rds='redis-server /usr/local/etc/redis.conf'
